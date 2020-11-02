8aa2ffd0b1762a50739428fb2bc02ed4a237dbb2
Susan McCarthy <susie.mccarthy98@gmail.com>
2020-11-02 15:54:19 +0000

    Menu expanded to include simple validation and return Strings based on what option is chosen

--- src/main/kotlin/asgn/astro/console/main/main.kt
+++ src/main/kotlin/asgn/astro/console/main/main.kt
@@ -11,7 +11,19 @@ fun main (args: Array<String>) {
     println("Susan McCarthy, 20080681")
 
     var input: Int
-    input = mainMenu()
+
+    do {
+        input = mainMenu()
+        when (input) {
+            1 -> println("You chose to create a new tracker list")
+            2 -> println("You chose to delete an existing tracker list")
+            3 -> println("You chose to view all existing tracker lists - this will prompt an internal menu")
+            -1 -> println("Exiting App, Goodbye :)")
+            else -> println("Invalid option, try again!")
+        }
+        println()
+    } while (input != -1)
+    logger.info { "Shutting Down AstroTracker App" }
 
 }
 
@@ -22,10 +34,15 @@ fun mainMenu() : Int {
 
     println("Welcome to the Main Menu")
     println("1. Create a new tracker list")
+    println("2. Delete an existing tracker list")
+    println("3. View all existing tracker lists")
     println("-1. Exit App")
     println("Please select an option: ")
     input = readLine()!! //takes in the user's choice
-    option = input.toInt() //converts it from a String to an Int
+    option = if (input.toIntOrNull() != null && !input.isEmpty()) //validates to check the input exists and is not null
+        input.toInt() //converts the input String to an Int
+    else
+        -9
 
     return option
 
