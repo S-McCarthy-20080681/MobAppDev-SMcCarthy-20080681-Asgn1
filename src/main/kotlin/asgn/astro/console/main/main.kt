1c655d7ce24d0512f8d6a67e04ec59ec1601ed5f
Susan McCarthy <susie.mccarthy98@gmail.com>
2020-11-02 14:51:06 +0000

    Basic skeleton main menu added

--- src/main/kotlin/asgn/astro/console/main/main.kt
+++ src/main/kotlin/asgn/astro/console/main/main.kt
@@ -1,6 +1,32 @@
 package asgn.astro.console.main
 
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
+
 fun main (args: Array<String>) {
+
+    logger.info { "Launching AstroTracker App" }
     println("AstroTracker App Version 1.0")
     println("Susan McCarthy, 20080681")
+
+    var input: Int
+    input = mainMenu()
+
+}
+
+fun mainMenu() : Int {
+
+    var option : Int
+    var input : String? = null
+
+    println("Welcome to the Main Menu")
+    println("1. Create a new tracker list")
+    println("-1. Exit App")
+    println("Please select an option: ")
+    input = readLine()!! //takes in the user's choice
+    option = input.toInt() //converts it from a String to an Int
+
+    return option
+
 }
