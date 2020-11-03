2e45c9bdbccc7e9a1323e5d25f119d797c175667
Susan McCarthy <susie.mccarthy98@gmail.com>
2020-11-03 15:57:06 +0000

    Made any declarations global. Added starter update features, and left filtering placeholders to come back and fill later

--- src/main/kotlin/asgn/astro/console/main/main.kt
+++ src/main/kotlin/asgn/astro/console/main/main.kt
@@ -7,6 +7,13 @@ import kotlin.system.exitProcess
 
 private val logger = KotlinLogging.logger {}
 
+var list = "" //to be replaced with an actual List later
+var title = "" //such as Perseids, total lunar eclipse, etc
+var category = "" //such as meteor shower, eclipse, etc
+var closestTime = "" //when that event will next happen; to be replaced by Date
+var nextTime = "" //when the event will happen again after the closest time; to be replaced by Date
+
+
 fun main (args: Array<String>) {
 
     logger.info { "Launching AstroTracker App" }
@@ -20,7 +27,9 @@ fun main (args: Array<String>) {
         when (input) {
             1 -> addList()
             2 -> deleteList()
-            3 -> internalMenu()
+            3 -> displayAllLists()
+            4 -> updateList()
+            5 -> internalMenu()
             0 -> {
                 println("Exiting App, Goodbye :)")
 
@@ -33,11 +42,11 @@ fun main (args: Array<String>) {
 
     var subInput: Int
 
-    while (input == 3) {
+    while (input == 5) {
         do {
             subInput = internalMenu()
             when (subInput) {
-                1 -> displayAllLists()
+                1 -> expandList()
                 2 -> addEvent()
                 3 -> deleteEvent()
                 4 -> updateEvent()
@@ -67,6 +76,8 @@ fun mainMenu() : Int {
     println("1. Create a new tracker list")
     println("2. Delete an existing tracker list")
     println("3. View all existing tracker lists")
+    println("4. Rename an existing list")
+    println("5. Event Editor Menu")
     println("0. Exit App")
     println("Please select an option: ")
     input = readLine()!! //takes in the user's choice
@@ -99,45 +110,67 @@ fun internalMenu(): Int {
 }
 
 fun addList() {
-    var list : String //to be replaced with an actual List later
 
     println("Create a new AstroTracker List")
     println()
+
     print("Name your list: ")
     list = readLine()!!
     println("You have created the AstroTracker list '$list'. Now add some events!")
-    internalMenu()
+
+    internalMenu() //brings the user back to the Edit Menu so they can add events to their new list
+
 }
 
 fun deleteList() {
     TODO()
 }
 
+fun updateList() {
+
+    println("Change the name of your AstroTracker List")
+    println()
+    //code to display all lists and ask user to choose one to rename (an "if {list.equals}" maybe???}
+    print("Current List name: $list")
+    print("Please choose a new List name: ")
+    list = readLine()!!
+    print("Your new List name is: $list")
+
+}
+
 fun displayAllLists() {
     println("This will return all lists currently stored by the app.")
 }
 
+fun expandList() {
+    TODO()
+}
+
 fun addEvent() {
-    var title : String? //such as Perseids, total lunar eclipse, etc
-    var category: String //such as meteor shower, eclipse, etc
-    var closestTime: String //when that event will next happen; to be replaced by Date
-    var nextTime: String //when the event will happen again after the closest time; to be replaced by Date
 
     println("Add an Event")
     println()
+
     print("What's happening?")
     title = readLine()!!
     println()
+
     print("Choose a category: Meteor Shower|Lunar Eclipse|Solar Eclipse|Supermoon")
     category = readLine()!!
     println()
-    print("When will it happen? dd/mm/yyyy")
+
+    print("When will it happen? [dd/mm/yyyy]")
     closestTime = readLine()!!
     println()
-    print("When will it next happen? dd/mm/yyyy")
-    nextTime = readLine()!!
 
-    internalMenu()
+    print("When will it next happen? [dd/mm/yyyy]")
+    nextTime = readLine()!!
+    println()
+
+    print("Here is your new Event: ")
+    print("Title: $title \n Category: $category \n Occurring: $closestTime \n Next Occurrence: $nextTime")
+
+    internalMenu() //brings the user back to the Edit Menu in case they want to add another event
 
 }
 
@@ -146,5 +179,31 @@ fun deleteEvent() {
 }
 
 fun updateEvent() {
-    TODO()
+
+    println("Update an Event")
+    println()
+
+    //code to ask user to choose a list, then display all events of that list for user to pick and update one
+
+    print("Enter a new name for your '$title' Event: ")
+    title = readLine()!!
+    println()
+
+    print("Enter a new category for your '$category' Event: Meteor Shower|Lunar Eclipse|Solar Eclipse|Supermoon")
+    category = readLine()!!
+    println()
+
+    print("Enter a new date for your Event on '$closestTime': [dd/mm/yyyy]")
+    closestTime = readLine()!!
+    println()
+
+    print("Currently, this Event will next occur on: $nextTime")
+    print("Enter a new next-date for your Event: [dd/mm/yyyy]")
+    nextTime = readLine()!!
+    println()
+
+    print("Here is your updated Event: ")
+    print("Title: $title \n Category: $category \n Occurring: $closestTime \n Next Occurrence: $nextTime")
+
+    internalMenu() //returns to Edit Menu
 }
